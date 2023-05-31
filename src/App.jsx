2d04a13bbb43e7fc22c4ba00ff75db3e64654eec
martindellugo <martindellugo@gmail.com>
2023-05-31 18:35:53 -0300

    base para examen Frontend 3

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './App.css'
+import Tarjeta from './Components/Tarjeta'
+import Form from './Components/Form'
+
+function App() {
+
+  
+  
+
+  return (
+    <>
+      <h1>Carga de estudiantes</h1>
+      
+      <Form />
+      <Tarjeta />
+
+      
+    </>
+  )
+}
+
+export default App
